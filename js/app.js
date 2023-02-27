35ac0d08538b68aa2ef3cd1042b35c5cf1f4d591
LucasNovoa <lucasnovoaok@gmail.com>
2023-02-27 17:06:36 -0300

    final commit

--- js/app.js
+++ js/app.js
@@ -11,18 +11,22 @@ document.getElementById('btnModo').onclick = ()=> {
     night = !night;
 };
 
+let user;
 fetch('https://randomuser.me/api/?gender=male')
       .then(response => response.json())
       .then(user => {
-        console.log(user.results[0])
-        document.getElementById('name').innerHTML = user.results[0].name.first + ' ' + user.results[0].name.last;
-        document.getElementById('profileImg').src = user.results[0].picture.large;
-        document.getElementById('email').innerHTML = '<i class="fas fa-envelope"></i> ' + user.results[0].email;
-        document.getElementById('phone').innerHTML = '<i class="fas fa-phone"></i> ' + user.results[0].phone;
-        document.getElementById('city').innerHTML = user.results[0].location.city + ', ' + user.results[0].location.country;
-        document.getElementById('birthday').innerHTML = user.results[0].dob.date.slice(8,10)+' / '+user.results[0].dob.date.slice(5,7)+' / '+user.results[0].dob.date.slice(0,4);
-        document.getElementById('emailReferencias').href = `mailto:${user.results[0].email}?Subject=Hola ${user.results[0].name.first}, me enviarías tus referencias laborales?`;
-      });
+        user = user.results[0]
+        console.log(user)
+        document.getElementById('name').innerHTML = user.name.first + ' ' + user.name.last;
+        document.getElementById('profileImg').src = user.picture.large;
+        document.getElementById('email').innerHTML = '<i class="fas fa-envelope"></i> ' + user.email;
+        document.getElementById('phone').innerHTML = '<i class="fas fa-phone"></i> ' + user.phone;
+        document.getElementById('city').innerHTML = user.location.city + ', ' + user.location.country;
+        document.getElementById('birthday').innerHTML = user.dob.date.slice(8,10)+' / '+user.dob.date.slice(5,7)+' / '+user.dob.date.slice(0,4);
+        document.getElementById('emailReferencias').href = `mailto:${user.email}?Subject=Hola ${user.name.first}, me enviarías tus referencias laborales?`;
+        let address = document.getElementById('address');
+        address.onclick = () => address.innerHTML === '( Ver dirección )' ? address.innerHTML = `${user.location.street.name} ${user.location.street.number}` : address.innerHTML = '( Ver dirección )';
+    });
 
 document.getElementById('profileImg').src;
 
